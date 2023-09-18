12cd53cf2041f3b35b55016d8924604d0d21dc6c
Ivan Kashirin <89252466+IvanWN@users.noreply.github.com>
2023-09-19 01:13:41 +0300

    Create benchmarks.h
    Create benchmarks.h file.

---
+++ benchmarks.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include <cmath>
+
+double f1(const std::vector<double>& position);
+double f2(const std::vector<double>& position);
+double f3(const std::vector<double>& position);
+double f4(const std::vector<double>& position);
+double f5(const std::vector<double>& position);
+double f6(const std::vector<double>& position);
+double f7(const std::vector<double>& position);
+double f8(const std::vector<double>& position);
+double f9(const std::vector<double>& position);
+double f10(const std::vector<double>& position);
+double f11(const std::vector<double>& position);
+double f12(const std::vector<double>& position)
+double f13(const std::vector<double>& position);
+double f14(const std::vector<double>& position);
