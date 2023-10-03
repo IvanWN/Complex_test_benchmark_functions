3e9a2f4b985823231edbfb7cc9d60370c9892e85
Ivan Kashirin <89252466+IvanWN@users.noreply.github.com>
2023-10-03 10:46:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <chrono>
 #include <numeric>
 #include <cmath>
-#include <string>
+#include <fstream>
+#include <iomanip>
 
 // Including files
 #include "SSA.cpp"
@@ -17,34 +18,19 @@
 using namespace std;
 
 template <typename AlgorithmFunc>
-void run_and_evaluate(const string& algorithm_name, AlgorithmFunc algorithm, int num_runs, const std::vector<std::pair<double, double>>& search_space) {
-    std::vector<double (*)(const std::vector<double>&)> benchmark_functions = {f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12};
-    std::vector<std::string> function_names = {"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12"};
+vector<vector<double>> run_and_evaluate(const string& algorithm_name, AlgorithmFunc algorithm, int num_runs, const std::vector<std::pair<double, double>>& search_space) {
+    std::vector<double (*)(const std::vector<double>&)> benchmark_functions = {f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13};
+    std::vector<vector<double>> all_results(benchmark_functions.size(), vector<double>(3, 0.0));
 
-    double (*mean_function)(const std::vector<double>&);
-    double (*stddev_function)(const std::vector<double>&, double);
-
-    // Select appropriate functions based on algorithm_name
-    if (algorithm_name == "ICA") {
-        mean_function = [](const std::vector<double>& vec) {
-            return std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
-        };
-        stddev_function = [](const std::vector<double>& vec, double mean_val) {
-            double sq_sum = std::inner_product(vec.begin(), vec.end(), vec.begin(), 0.0);
-            double sq_mean = sq_sum / vec.size();
-            return std::sqrt(sq_mean - mean_val * mean_val);
-        };
-    } else {
-        mean_function = [](const std::vector<double>& values) {
-            return std::accumulate(values.begin(), values.end(), 0.0) / values.size();
-        };
-        stddev_function = [](const std::vector<double>& values, double mean_val) {
-            double variance = std::accumulate(values.begin(), values.end(), 0.0, [mean_val](double acc, double val) {
-                return acc + (val - mean_val) * (val - mean_val);
-            }) / values.size();
-            return std::sqrt(variance);
-        };
-    }
+    double (*mean_function)(const std::vector<double>&) = [](const std::vector<double>& values) {
+        return std::accumulate(values.begin(), values.end(), 0.0) / values.size();
+    };
+    double (*stddev_function)(const std::vector<double>&, double) = [](const std::vector<double>& values, double mean_val) {
+        double variance = std::accumulate(values.begin(), values.end(), 0.0, [mean_val](double acc, double val) {
+            return acc + (val - mean_val) * (val - mean_val);
+        }) / values.size();
+        return std::sqrt(variance);
+    };
 
     for (size_t func_idx = 0; func_idx < benchmark_functions.size(); ++func_idx) {
         auto& benchmark_function = benchmark_functions[func_idx];
@@ -60,48 +46,66 @@ void run_and_evaluate(const string& algorithm_name, AlgorithmFunc algorithm, int
             fitness_results.push_back(fitness);
             run_times.push_back(run_time.count());
         }
-
-        double mean_fitness = mean_function(fitness_results);
-        double fitness_std_dev = stddev_function(fitness_results, mean_fitness);
-        double mean_run_time = mean_function(run_times);
-
-        // Display results for each benchmark function
-        std::cout << "Algorithm: " << algorithm_name << " | Benchmark function: " << function_names[func_idx] << std::endl;
-        std::cout << "Average fitness: " << mean_fitness << std::endl;
-        std::cout << "Standard deviation: " << fitness_std_dev << std::endl;
-        std::cout << "Average run time: " << mean_run_time << " sec" << std::endl;
-        std::cout << "---------------------------------------------\n";
-        std::cout << std::endl;
+        all_results[func_idx][0] = mean_function(fitness_results);
+        all_results[func_idx][1] = stddev_function(fitness_results, all_results[func_idx][0]);
+        all_results[func_idx][2] = mean_function(run_times);
     }
+    return all_results;
 }
 
 int main() {
     const int num_runs = 30;
     std::vector<std::pair<double, double>> search_space(20, {-10.0, 10.0});
 
-    run_and_evaluate("SSA", [&search_space](auto benchmark_function) {
-        return ssa(100, 3000, search_space, benchmark_function);
-    }, num_runs, search_space);
+    std::vector<std::string> algorithm_names = {"WOA", "AOA", "SSA", "ICA", "TLBO", "SCA"};
+    std::vector<std::vector<vector<double>>> all_algorithm_results;
 
-    run_and_evaluate("ICA", [&search_space](auto benchmark_function) {
-        return ica(100, 3000, search_space, 10, benchmark_function);
-    }, num_runs, search_space);
+    all_algorithm_results.push_back(run_and_evaluate("WOA", [&search_space](auto benchmark_function) {
+        return woa(100, 3000, search_space, benchmark_function);
+    }, num_runs, search_space));
 
-    run_and_evaluate("TLBO", [&search_space](auto benchmark_function) {
-        return tlbo(100, 3000, search_space, benchmark_function);
-    }, num_runs, search_space);
-
-    run_and_evaluate("SCA", [&search_space](auto benchmark_function) {
-        return sca(100, 3000, search_space, benchmark_function);
-    }, num_runs, search_space);
-
-    run_and_evaluate("AOA", [&search_space](auto benchmark_function) {
+    all_algorithm_results.push_back(run_and_evaluate("AOA", [&search_space](auto benchmark_function) {
         return aoa(100, 3000, search_space, benchmark_function);
-    }, num_runs, search_space);
-    
-    run_and_evaluate("WOA", [&search_space](auto benchmark_function) {
+    }, num_runs, search_space));
+
+    all_algorithm_results.push_back(run_and_evaluate("SSA", [&search_space](auto benchmark_function) {
+        return ssa(100, 3000, search_space, benchmark_function);
+    }, num_runs, search_space));
+
+    all_algorithm_results.push_back(run_and_evaluate("ICA", [&search_space](auto benchmark_function) {
+        return ica(100, 3000, search_space, 10, benchmark_function); // 10 - это пример значения для num_empires
+    }, num_runs, search_space));
+
+    all_algorithm_results.push_back(run_and_evaluate("TLBO", [&search_space](auto benchmark_function) {
         return tlbo(100, 3000, search_space, benchmark_function);
-    }, num_runs, search_space);
+    }, num_runs, search_space));
+
+    all_algorithm_results.push_back(run_and_evaluate("SCA", [&search_space](auto benchmark_function) {
+        return sca(100, 3000, search_space, benchmark_function);
+    }, num_runs, search_space));
+
+    std::ofstream outfile("results.txt");
+
+    if (!outfile.is_open()) {
+        std::cerr << "Error opening results.txt for writing" << std::endl;
+        return 1;
+    }
+
+    outfile << std::fixed << std::setprecision(2);
+    for (const auto& name : algorithm_names)
+        outfile << std::setw(15) << name;
+    outfile << '\n';
+
+    for (size_t func_idx = 0; func_idx < 13; ++func_idx) {
+        outfile << "f" << func_idx + 1 << ": ";
+        for (const auto& alg_results : all_algorithm_results) {
+            for (const auto& result : alg_results[func_idx])
+                outfile << std::setw(15) << result;
+        }
+        outfile << '\n';
+    }
+
+    outfile.close();
 
     return 0;
 }
