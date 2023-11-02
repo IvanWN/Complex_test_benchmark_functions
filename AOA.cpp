4e85b05961d4457499e0965256535b67a7218c41
Ivan Kashirin <89252466+IvanWN@users.noreply.github.com>
2023-11-02 16:04:41 +0300

    Update AOA.cpp

--- AOA.cpp
+++ AOA.cpp
@@ -6,25 +6,27 @@
 #include <ctime>
 #include "benchmarks.h"
 
+// Creating a random number generator based on the current time
 std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
 
+// Function for updating the agent's position based on time and search space
 std::vector<double> updatePosition(const std::vector<double>& agent, double t, const std::vector<std::pair<double, double>>& search_space) {
     std::vector<double> new_position(agent.size());
     for(size_t i = 0; i < agent.size(); i++) {
         double amplitude = (search_space[i].second - search_space[i].first) / 2.0;
         new_position[i] = agent[i] + amplitude * std::sin(2 * M_PI * t);
-        // Обеспечиваем, что новая позиция находится в пределах пространства поиска
         new_position[i] = std::clamp(new_position[i], search_space[i].first, search_space[i].second);
     }
     return new_position;
 }
 
+// The main function for optimization based on the algorithm
 std::pair<std::vector<double>, double> aoa(int num_agents, int max_iter, const std::vector<std::pair<double, double>>& search_space,
                                            double (*benchmark_function)(const std::vector<double>&)) {
     int num_features = search_space.size();
     std::vector<std::vector<double>> agents(num_agents, std::vector<double>(num_features));
 
-    // Инициализация популяции
+    // Initialization of the population
     for(int i = 0; i < num_agents; i++) {
         for(int j = 0; j < num_features; j++) {
             agents[i][j] = getRandomValue(search_space[j].first, search_space[j].second);
@@ -33,13 +35,13 @@ std::pair<std::vector<double>, double> aoa(int num_agents, int max_iter, const s
 
     std::vector<double> fitness(num_agents, std::numeric_limits<double>::infinity());
 
-    // Применение функции benchmark_function для инициализированных агентов
+    // Using the benchmark_function function for initialized agents
     std::for_each(agents.begin(), agents.end(), [&](const std::vector<double>& agent) {
         int i = &agent - &agents[0];
         fitness[i] = benchmark_function(agent);
     });
 
-    // Основной цикл оптимизации
+    // Main optimization cycle
     for (int t = 0; t < max_iter; t++) {
         double time_ratio = static_cast<double>(t) / max_iter;
 
@@ -52,7 +54,8 @@ std::pair<std::vector<double>, double> aoa(int num_agents, int max_iter, const s
             }
         }
     }
-
+    
+    // Search for the best result among all agents
     auto min_element_iter = std::min_element(fitness.begin(), fitness.end());
     int best_index = std::distance(fitness.begin(), min_element_iter);
     return {agents[best_index], *min_element_iter};
